6e76ea28e01025a97b5cad7af33c066804c0336a
Aidan <iiiaiid4niii@gmail.com>
2023-03-23 12:23:51 +0000

    changed map type

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,39 +1,40 @@
-
-import {
-  MapContainer,
-  TileLayer,
-  Circle,
-  Popup,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Circle, Popup } from "react-leaflet";
 import { useState, useEffect } from "react";
 
-
-navigator.geolocation.getCurrentPosition(data => console.log(data))
+//google https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}
+//openStreetMap https://tile.openstreetmap.org/{z}/{x}/{y}.png
 
 const Map = () => {
-    const [startCoords, setStartCoords] = useState([51.5, -0,55])
+  const [startCoords, setStartCoords] = useState([51.5, -0, 55]);
 
-    useEffect(() => {
-        navigator.geolocation.watchPosition(data => setStartCoords([data.coords.latitude,data.coords.longitude ]), () => {}, {enableHighAccuracy: true})
-    })
+  useEffect(() => {
+    navigator.geolocation.watchPosition(
+      (data) => setStartCoords([data.coords.latitude, data.coords.longitude]),
+      () => {},
+      { enableHighAccuracy: true }
+    );
+  });
 
-return (<MapContainer
+  return (
+    <MapContainer
+      center={startCoords}
+      zoom={16}
+      scrollWheelZoom={true}
+      id="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+      />
+      <Circle
         center={startCoords}
-        zoom={16}
-        scrollWheelZoom={true}
-        id="map"
+        pathOptions={{ color: "green", stroke: false }}
+        radius={50}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
-        />
-        <Circle
-          center={startCoords}
-          pathOptions={{ color: "green", stroke: false }}
-          radius={50}
-        >
-          <Popup></Popup>
-        </Circle>
-      </MapContainer>)}
+        <Popup></Popup>
+      </Circle>
+    </MapContainer>
+  );
+};
 
-      export default Map
+export default Map;

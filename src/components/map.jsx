d4816631b8c718011908a272353b6d9895c14bb5
Aidan <iiiaiid4niii@gmail.com>
2023-03-23 13:51:34 +0000

    added loading for map

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -6,35 +6,37 @@ import { useState, useEffect } from "react";
 
 const Map = () => {
   const [startCoords, setStartCoords] = useState([51.5, -0, 55]);
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     navigator.geolocation.watchPosition(
-      (data) => setStartCoords([data.coords.latitude, data.coords.longitude]),
+      (data) => {
+        setIsLoading(false)
+        setStartCoords([data.coords.latitude, data.coords.longitude])},
       () => {},
       { enableHighAccuracy: true }
     );
   }, []);
 
-  return (
-    <MapContainer
+
+  return isLoading ? <h1>Loading</h1> : ( <MapContainer
+    center={startCoords}
+    zoom={16}
+    scrollWheelZoom={true}
+    id="map"
+  >
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Circle
       center={startCoords}
-      zoom={16}
-      scrollWheelZoom={true}
-      id="map"
+      pathOptions={{ color: "green", stroke: false }}
+      radius={50}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
-      />
-      <Circle
-        center={startCoords}
-        pathOptions={{ color: "green", stroke: false }}
-        radius={50}
-      >
-        <Popup></Popup>
-      </Circle>
-    </MapContainer>
-  );
+      <Popup></Popup>
+    </Circle>
+  </MapContainer>)
 };
 
 export default Map;
